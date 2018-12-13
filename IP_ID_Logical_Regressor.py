e8b1638128dbb68dce236e6e4cbe53c8a19befdb
Md. Ahsan Ayub <md.ahsanayub@outlook.com>
2018-12-12 19:21:30 -0600

    Last Updated from Tech machine

--- IP_ID_Logical_Regressor.py
+++ IP_ID_Logical_Regressor.py
@@ -8,12 +8,11 @@ __status__ = "Prototype"
 
 #importing the library
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 #importing the data set
-dataset = pd.read_excel("Final_Processed_Data_Set_IP_ID_22_10_2018.xlsx")
-
-# Dropping the features utilizing domain knowledge and feature selection
-dataset = dataset.drop(['ip.id', 'ip.checksum'], axis = 1)
+dataset = pd.read_excel("Updated_Final_Processed_Data_Set_IP_ID_12_12_2018.xlsx")
 
 # X and Y
 Y = dataset.iloc[:, -1].values
@@ -43,3 +42,51 @@ cm = confusion_matrix(Y_test, Y_pred)
 # Knowing accuracy result
 from sklearn.metrics import accuracy_score
 print(accuracy_score(Y_test, Y_pred))
+
+def plot_confusion_matrix(cm,
+                          target_names,
+                          title='Confusion matrix',
+                          cmap=None,
+                          normalize=True):
+    import itertools
+
+    accuracy = np.trace(cm) / float(np.sum(cm))
+    misclass = 1 - accuracy
+
+    if cmap is None:
+        cmap = plt.get_cmap('Blues')
+
+    plt.figure(figsize=(8, 6))
+    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+    plt.title(title)
+    #plt.colorbar()
+
+    if target_names is not None:
+        tick_marks = np.arange(len(target_names))
+        plt.xticks(tick_marks, target_names, rotation=45)
+        plt.yticks(tick_marks, target_names)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+
+    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        if normalize:
+            plt.text(j, i, "{:0.4f}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+        else:
+            plt.text(j, i, "{:,}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+
+
+    plt.tight_layout()
+    plt.xlabel('\nAccuracy={:0.4f}; Misclass={:0.4f}'.format(accuracy, misclass))
+    plt.show()
+    
+plot_confusion_matrix(cm, 
+                      normalize    = False,
+                      target_names = ['0', '1'],
+                      title        = "Logistic Regression Classifier in Network Layer")
---
+++ TCP_K-NN.py
@@ -0,0 +1,97 @@
+__author__ = "Md. Ahsan Ayub"
+__license__ = "GPL"
+__credits__ = ["Ayub, Md. Ahsan", "Smith, Steven", "Siraj, Ambareen",
+                    "Hope, Shelby"]
+__maintainer__ = "Md. Ahsan Ayub"
+__email__ = "mayub42@students.tntech.edu"
+__status__ = "Prototype"
+
+#importing the library
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#importing the data set
+dataset = pd.read_excel("Updated_Final_Processed_Data_Set_TCP_Seq_12_12_2018.xlsx")
+
+# X and Y
+Y = dataset.iloc[:, -1].values
+X = dataset.iloc[:,:-1].values
+
+# Spliting the dataset into the Training and Test Set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.35, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+# Fitting K-NN to the Training Set
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors=5,metric='minkowski',p=2)
+classifier.fit(X_train, Y_train)
+
+# Predicting the Test set results
+Y_pred = classifier.predict(X_test)
+
+# Making the confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(Y_test, Y_pred)
+
+# Knowing accuracy result
+from sklearn.metrics import accuracy_score
+print(accuracy_score(Y_test, Y_pred))
+
+
+
+
+def plot_confusion_matrix(cm,
+                          target_names,
+                          title='Confusion matrix',
+                          cmap=None,
+                          normalize=True):
+    import itertools
+
+    accuracy = np.trace(cm) / float(np.sum(cm))
+    misclass = 1 - accuracy
+
+    if cmap is None:
+        cmap = plt.get_cmap('Blues')
+
+    plt.figure(figsize=(8, 6))
+    #plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.imshow(cm, cmap=cmap)
+    plt.title(title)
+    #plt.colorbar()
+
+    if target_names is not None:
+        tick_marks = np.arange(len(target_names))
+        plt.xticks(tick_marks, target_names, rotation=45)
+        plt.yticks(tick_marks, target_names)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+
+    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        if normalize:
+            plt.text(j, i, "{:0.4f}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+        else:
+            plt.text(j, i, "{:,}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+
+
+    plt.tight_layout()
+    plt.xlabel('\nAccuracy={:0.4f}; Misclass={:0.4f}'.format(accuracy, misclass))
+    plt.show()
+    
+plot_confusion_matrix(cm, 
+                      normalize    = False,
+                      target_names = ['0', '1'],
+                      title        = "K-NN Classifier")
